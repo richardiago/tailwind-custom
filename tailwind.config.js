c5a047ecca7044ae820c3d533418d36c965f47d7
Iago Richard <iagorichard@outlook.com>
2023-12-11 21:15:51 -0300

    custom

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+
+const {colors} = require('tailwindcss/defaultTheme')
+
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				'new-blue': '#243C5A',
+				gray: {
+					...colors.gray,
+					'900': '#999'
+				}
+			}, 
+			spacing: {
+				'50': '20rem',
+			}
+		},
+	},
+	plugins: [],
+}
+
